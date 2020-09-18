7a638062c627814ba8c096c8d69b6457f2df5336
smith@ariveguru.com <smith@ariveguru.com>
2020-09-18 20:34:41 +0530

    basic landing page added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './App.css';
+
+import Login from './Login';
+function App() {
+	return (
+		// BEM
+		<div className="app">
+			<Login />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/spotify.js
@@ -0,0 +1,15 @@
+// export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// const redirectUri = 'http://localhost:3000';
+
+// const clientId = '242283d9984846c19b76b0b96c521d8f';
+
+// const scopes = [
+// 	'user-read-currently-playing',
+// 	'user-read-recently-played',
+// 	'user-read-playback-state',
+// 	'user-top-read',
+// 	'user-modify-playback-state',
+// ];
+
+// export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join()}`;
