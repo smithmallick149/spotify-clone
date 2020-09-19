e83cfbc5f9dc7dbca061554ad2ca5bf2ee01db55
smith@ariveguru.com <smith@ariveguru.com>
2020-09-19 15:43:48 +0530

    getting user permissions  by setting up a url

--- src/spotify.js
+++ src/spotify.js
@@ -1,15 +1,17 @@
-// export const authEndpoint = 'https://accounts.spotify.com/authorize';
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
 
-// const redirectUri = 'http://localhost:3000';
+const redirectUri = 'http://localhost:3000';
 
-// const clientId = '242283d9984846c19b76b0b96c521d8f';
+const clientId = '242283d9984846c19b76b0b96c521d8f';
 
-// const scopes = [
-// 	'user-read-currently-playing',
-// 	'user-read-recently-played',
-// 	'user-read-playback-state',
-// 	'user-top-read',
-// 	'user-modify-playback-state',
-// ];
+const scopes = [
+	'user-read-currently-playing',
+	'user-read-recently-played',
+	'user-read-playback-state',
+	'user-top-read',
+	'user-modify-playback-state',
+];
 
-// export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join()}`;
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+	'%20'
+)}&response_type=token&show_dialog=true`;
